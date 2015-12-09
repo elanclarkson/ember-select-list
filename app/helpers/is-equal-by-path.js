13ef83094f91ac08afbbe85ad144216fe568e1ff
Elan <elan@applicable.co.nz>
2015-12-09 14:02:40 +1300

    Change default value comparison logic to not do type comparison so that IDs can be more accurately compared

--- app/helpers/is-equal-by-path.js
+++ app/helpers/is-equal-by-path.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Helper.helper(function([leftSide, rightSide, path]) {
   if (path) {
-    return Ember.get(leftSide, path) === rightSide;
+    return Ember.get(leftSide, path) == rightSide;
   } else {
     return leftSide === rightSide;
   }
