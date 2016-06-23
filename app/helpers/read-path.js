237e7e9778048406c831ef377f272e076f973320
Elan <elan@applicable.co.nz>
2016-06-23 15:05:46 +1200

    Replace read-path helper with Ember get helper so that binding works correctly. See bug report #13078 for more info https://github.com/emberjs/ember.js/issues/13078

--- app/helpers/read-path.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Helper.helper(function([object, path]){
-  if (path) {
-    return Ember.get(object, path);
-  } else {
-    return object;
-  }
-});
